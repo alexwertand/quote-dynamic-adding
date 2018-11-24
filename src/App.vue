9ce8a238ac2ec7f2be3006e0eac3a4a071d71924
Alex And <alexandr_works@ukr.net>
2018-11-25 00:34:00 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="container">
+			<app-header :quotesCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+
+			<app-new-quote @quoteAdded="newQuote"></app-new-quote>
+
+			<app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="alert alert-info">Info: Click in a Quote to delete it! </div>
+				</div>
+			</div>
+
+			<hr>
+
+			<hr>
+
+			<hr>
+
+			<app-simple-form></app-simple-form>
+
+			<hr>
+
+			<hr>
+
+			<hr>
+
+			<hr>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	//import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	import QuoteGrid from './components/QuoteGrid.vue'
+	import NewQuote from './components/NewQuote.vue'
+	import Header from './components/Header.vue'
+	import SimpleForm from './components/SimpleForm.vue'
+
+	export default {
+		name: 'app',
+		data() {
+			return {
+				quotes: [
+					'Just a Quote to see something'
+				],
+				maxQuotes: 10
+			}
+		},
+		methods: {
+			newQuote(quote) {
+				if (this.quotes.length >= this.maxQuotes) {
+					return alert('Please delete Quotes first!');
+				}
+
+				this.quotes.push(quote);
+			},
+			deleteQuote(index) {
+				this.quotes.splice(index, 1);
+			}
+		},
+		components: {
+			appHeader: Header,
+			appNewQuote: NewQuote,
+			appQuoteGrid: QuoteGrid,
+			appSimpleForm: SimpleForm
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+            <h3>Quote</h3>
+
+            <textarea class="form-control" rows="5" v-model="quote"></textarea>
+        </div>
+
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+            <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['quotes'],
+        data() {
+            return{
+                quote: ''
+            }
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
